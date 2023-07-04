5e0b21cd00125d2b3d17012bc06d7ea399475d2c
MikelVT <mikeltan30@gmail.com>
2023-07-04 19:28:39 +0700

    kelarin validasi login

--- app/src/main/java/com/vacash/android/LoginPage.java
+++ app/src/main/java/com/vacash/android/LoginPage.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -30,9 +31,30 @@ public class LoginPage extends AppCompatActivity {
         logInBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                if(emailField.getText().toString().isEmpty()){
+                String email = emailField.getText().toString();
+                String password = passwordField.getText().toString();
+
+                if(email.isEmpty()){
                     errorMsg.setText("* Email cannot be empty.");
                     errorMsg.setVisibility(View.VISIBLE);
+                } else if (password.isEmpty()) {
+                    errorMsg.setText("* Password cannot be empty.");
+                    errorMsg.setVisibility(View.VISIBLE);
+                } else if (!email.endsWith(".com")) {
+                    errorMsg.setText("* Email must contain \".com\".");
+                    errorMsg.setVisibility(View.VISIBLE);
+                } else if (!email.contains("@")) {
+                    errorMsg.setText("* Email must contain \"@\".");
+                    errorMsg.setVisibility(View.VISIBLE);
+                } else if (email.indexOf("@") != email.lastIndexOf("@")) {
+                    errorMsg.setText("* Email must contain only one \"@\".");
+                    errorMsg.setVisibility(View.VISIBLE);
+                } else if (password.length() < 8) {
+                    errorMsg.setText("* Password cannot be less than 8 characters.");
+                    errorMsg.setVisibility(View.VISIBLE);
+                } else {
+                    errorMsg.setVisibility(View.INVISIBLE);
+                    Toast.makeText(LoginPage.this, "Berhasil", Toast.LENGTH_SHORT).show();
                 }
             }
         });
