025869d3f8d8ff707894a07b4a70d00176d1b051
MikelVT <mikeltan30@gmail.com>
2023-07-04 18:55:43 +0700

    tambah validasi dikit

--- app/src/main/java/com/vacash/android/LoginPage.java
+++ app/src/main/java/com/vacash/android/LoginPage.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -18,5 +21,20 @@ public class LoginPage extends AppCompatActivity {
         // setting app name text color
         TextView appName =  findViewById(R.id.appName);
         appName.setText(Html.fromHtml("<b><font color='#8453D5'>V</font>" + "<font color='#A479EB'>A</font>" + "<font color='#FFFFFF'> Cash</font></b>"));
+
+        // validation
+        EditText emailField = findViewById(R.id.emailField);
+        EditText passwordField = findViewById(R.id.passwordField);
+        Button logInBtn = findViewById(R.id.logInBtn);
+        TextView errorMsg = findViewById(R.id.errorMessage);
+        logInBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(emailField.getText().toString().isEmpty()){
+                    errorMsg.setText("* Email cannot be empty.");
+                    errorMsg.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 }
