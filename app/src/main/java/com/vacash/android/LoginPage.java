ea48eb663212dbf9d9de51d37ec878497be765fe
MikelVT <mikeltan30@gmail.com>
2023-07-04 18:36:50 +0700

    benerin login dikit

--- app/src/main/java/com/vacash/android/LoginPage.java
+++ app/src/main/java/com/vacash/android/LoginPage.java
@@ -3,6 +3,10 @@ package com.vacash.android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +14,9 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setting app name text color
+        TextView appName =  findViewById(R.id.appName);
+        appName.setText(Html.fromHtml("<b><font color='#8453D5'>V</font>" + "<font color='#A479EB'>A</font>" + "<font color='#FFFFFF'> Cash</font></b>"));
     }
 }
