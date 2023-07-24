b6865d9d0a4366febe627d9054a1fcaaf1f61610
stevensumadi <stevensumadi@gmail.com>
2023-07-24 13:01:05 +0700

    itempage2

--- app/src/main/java/com/vacash/android/ItemPage.java
+++ app/src/main/java/com/vacash/android/ItemPage.java
@@ -1,7 +1,17 @@
 package com.vacash.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
+
+import com.vacash.android.adapters.ItemAdapter;
+import com.vacash.android.adapters.PurchaseHistoryAdapter;
+import com.vacash.android.models.Item;
+import com.vacash.android.models.PurchaseHistory;
+
+import java.util.ArrayList;
 
 public class ItemPage extends AppCompatActivity {
 
@@ -9,5 +19,17 @@ public class ItemPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_page);
+
+        ArrayList<Item> listOfItems = new ArrayList<>();
+        listOfItems.add(new Item("Codashop", "60 Genesis Crystal", 16500, R.drawable.item_genesis_crystal));
+        listOfItems.add(new Item("Codashop", "60 Genesis Crystal", 16500, R.drawable.item_genesis_crystal));
+        listOfItems.add(new Item("Codashop", "60 Genesis Crystal", 16500, R.drawable.item_genesis_crystal));
+        listOfItems.add(new Item("Codashop", "60 Genesis Crystal", 16500, R.drawable.item_genesis_crystal));
+        listOfItems.add(new Item("Codashop", "60 Genesis Crystal", 16500, R.drawable.item_genesis_crystal));
+
+        RecyclerView itemsRecycleView = findViewById(R.id.ItemRecycleView);
+        itemsRecycleView.setLayoutManager(new LinearLayoutManager(this));
+        itemsRecycleView.setAdapter(new ItemAdapter(
+                listOfItems));
     }
 }
---
+++ app/src/main/java/com/vacash/android/adapters/ItemAdapter.java
@@ -0,0 +1,61 @@
+package com.vacash.android.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.vacash.android.R;
+import com.vacash.android.models.Item;
+
+import java.util.List;
+
+public class ItemAdapter  extends RecyclerView.Adapter<ItemAdapter.ViewHolder>{
+
+    List<Item> items;
+
+    public ItemAdapter(List<Item> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ItemAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ItemAdapter.ViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_view, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemAdapter.ViewHolder holder, int position) {
+        holder.itemsImage.setImageResource(items.get(position).getItemsImage());
+        holder.shopName.setText(items.get(position).getShopName());
+        holder.itemsName.setText(items.get(position).getItemsName());
+
+        String totalPriceText = "IDR " + items.get(position).getItemsPrice().toString();
+        holder.itemsPrice.setText(totalPriceText);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView itemsImage;
+        private TextView shopName, itemsName, itemsPrice;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            itemsImage = itemView.findViewById(R.id.itemsImage);
+            shopName = itemView.findViewById(R.id.shopName);
+            itemsName = itemView.findViewById(R.id.itemsName);
+            itemsPrice = itemView.findViewById(R.id.itemsPrice);
+        }
+
+    }
+}
